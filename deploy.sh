f23cd2ebef14985040d998b7df855d4b1af906f7
Kheeran <kn16063@bristol.ac.uk>
2021-06-24 12:03:59 +0100

    Deploy

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+git checkout dev
+git pull
+commit_id=$(git rev-parse HEAD)
+bundle install
+bundle exec jekyll build
+echo "BUILD COMPLETE!"
+git checkout master
+rm -r !("_site")
+mv _site/* ./
+rm -d _site
+git add .
+git commit -m "Deploy: $(date) $commit_id"
+git push origin master
+git checkout dev
+echo "WEBSITE DEPLOYED!"
